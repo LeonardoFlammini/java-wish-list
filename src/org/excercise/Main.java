818f8f39fa52e83447725917b4299426d62e7a18
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-23 14:59:48 +0200

    scrittura su file

--- src/org/excercise/Main.java
+++ src/org/excercise/Main.java
@@ -3,17 +3,23 @@ package org.excercise;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Variabili
         Scanner scanner = new Scanner(System.in);
+
+        File filepath = new File("./resources/list.txt");
 
         ArrayList<String> presents = new ArrayList<>();
 
         boolean stop = false;
-
-
+        //
+        //Eseguibile
         while(!stop){
             System.out.println("What present do you want?");
             String present = scanner.nextLine();
@@ -35,11 +41,36 @@ public class Main {
             }
         }
 
+        for( String present : presents){
+            writeToFile(present,filepath);
+        }
+
 
 
 
 
 
         scanner.close();
+    }
+
+    //Metodi
+    private static void writeToFile(String s, File file) {
+        // try-with-resources
+        try (FileWriter fileWriter = new FileWriter(file, true)) { // Aggiunto 'true' per abilitare l'append
+            fileWriter.write(s + "\n"); // Aggiunto '\n' per separare i libri nel file
+        } catch (IOException e) {
+            System.out.println("Unable to write to file: " + e.getMessage());
+        }
+    }
+
+    private static void readFile(File file) {
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to read file");
+        }
     }
 }
